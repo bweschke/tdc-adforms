66444133887a9a73e6c650039deeda5231d7d36f
BJ Weschke <bweschke@btwtech.com>
2017-08-22 12:09:12 -0400

    Initial commit

---
+++ src/main/java/com/dbpfootball/adsform/FormprocessorApplication.java
@@ -0,0 +1,12 @@
+package com.dbpfootball.adsform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormprocessorApplication.class, args);
+	}
+}
