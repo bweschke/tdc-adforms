48435f49b00bccd07ff47e1f466af1f37978355b
BJ Weschke <bweschke@btwtech.com>
2017-10-12 22:02:14 -0400

    Vineyard Vines order form

---
+++ src/main/java/com/dbpfootball/adsform/domain/vvinesformsubmission.java
@@ -0,0 +1,407 @@
+package com.dbpfootball.adsform.domain;
+
+public class vvinesformsubmission {
+	
+	String contactname;
+	String address;
+	String email;
+	String phone;
+	String ccfname;
+	String cclname;
+	String ccaddress;
+	String cccity;
+	String ccstate;
+	String cczip;
+	String cctype;
+	String ccnum;
+	String ccexpmo;
+	String ccexpyr;
+	String cccvv;
+	
+	String tie1qty;
+	String tie2qty;
+	String hat1qty;
+	String hat2qty;
+	
+	String belt1size;
+	String belt1qty;
+	String belt2size;
+	String belt2qty;
+	String belt3size;
+	String belt3qty;
+	
+	String mshep1size;
+	String mshep1qty;
+	String mshep2size;
+	String mshep2qty;
+	String mshep3size;
+	String mshep3qty;
+	
+	String wshep1size;
+	String wshep1qty;
+	String wshep2size;
+	String wshep2qty;
+	String wshep3size;
+	String wshep3qty;
+	
+	String qtrzipgrey1size;
+	String qtrzipgrey1qty;
+	String qtrzipgrey2size;
+	String qtrzipgrey2qty;
+	String qtrzipgrey3size;
+	String qtrzipgrey3qty;
+	
+	String qtrzipblack1size;
+	String qtrzipblack1qty;
+	String qtrzipblack2size;
+	String qtrzipblack2qty;
+	String qtrzipblack3size;
+	String qtrzipblack3qty;
+	
+	String tshirt1size;
+	String tshirt1qty;
+	String tshirt2size;
+	String tshirt2qty;
+	String tshirt3size;
+	String tshirt3qty;
+	
+	String totalvalue;
+	
+	public String getContactname() {
+		return contactname;
+	}
+	public void setContactname(String contactname) {
+		this.contactname = contactname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCcfname() {
+		return ccfname;
+	}
+	public void setCcfname(String ccfname) {
+		this.ccfname = ccfname;
+	}
+	public String getCclname() {
+		return cclname;
+	}
+	public void setCclname(String cclname) {
+		this.cclname = cclname;
+	}
+	public String getCcaddress() {
+		return ccaddress;
+	}
+	public void setCcaddress(String ccaddress) {
+		this.ccaddress = ccaddress;
+	}
+	public String getCccity() {
+		return cccity;
+	}
+	public void setCccity(String cccity) {
+		this.cccity = cccity;
+	}
+	public String getCcstate() {
+		return ccstate;
+	}
+	public void setCcstate(String ccstate) {
+		this.ccstate = ccstate;
+	}
+	public String getCczip() {
+		return cczip;
+	}
+	public void setCczip(String cczip) {
+		this.cczip = cczip;
+	}
+	public String getCctype() {
+		return cctype;
+	}
+	public void setCctype(String cctype) {
+		this.cctype = cctype;
+	}
+	public String getCcnum() {
+		return ccnum;
+	}
+	public void setCcnum(String ccnum) {
+		this.ccnum = ccnum;
+	}
+	public String getCcexpmo() {
+		return ccexpmo;
+	}
+	public void setCcexpmo(String ccexpmo) {
+		this.ccexpmo = ccexpmo;
+	}
+	public String getCcexpyr() {
+		return ccexpyr;
+	}
+	public void setCcexpyr(String ccexpyr) {
+		this.ccexpyr = ccexpyr;
+	}
+	public String getCccvv() {
+		return cccvv;
+	}
+	public void setCccvv(String cccvv) {
+		this.cccvv = cccvv;
+	}
+	public String getTie1qty() {
+		return tie1qty;
+	}
+	public void setTie1qty(String tie1qty) {
+		this.tie1qty = tie1qty;
+	}
+	public String getTie2qty() {
+		return tie2qty;
+	}
+	public void setTie2qty(String tie2qty) {
+		this.tie2qty = tie2qty;
+	}
+	public String getHat1qty() {
+		return hat1qty;
+	}
+	public void setHat1qty(String hat1qty) {
+		this.hat1qty = hat1qty;
+	}
+	public String getHat2qty() {
+		return hat2qty;
+	}
+	public void setHat2qty(String hat2qty) {
+		this.hat2qty = hat2qty;
+	}
+	public String getBelt1size() {
+		return belt1size;
+	}
+	public void setBelt1size(String belt1size) {
+		this.belt1size = belt1size;
+	}
+	public String getBelt1qty() {
+		return belt1qty;
+	}
+	public void setBelt1qty(String belt1qty) {
+		this.belt1qty = belt1qty;
+	}
+	public String getBelt2size() {
+		return belt2size;
+	}
+	public void setBelt2size(String belt2size) {
+		this.belt2size = belt2size;
+	}
+	public String getBelt2qty() {
+		return belt2qty;
+	}
+	public void setBelt2qty(String belt2qty) {
+		this.belt2qty = belt2qty;
+	}
+	public String getBelt3size() {
+		return belt3size;
+	}
+	public void setBelt3size(String belt3size) {
+		this.belt3size = belt3size;
+	}
+	public String getBelt3qty() {
+		return belt3qty;
+	}
+	public void setBelt3qty(String belt3qty) {
+		this.belt3qty = belt3qty;
+	}
+	public String getMshep1size() {
+		return mshep1size;
+	}
+	public void setMshep1size(String mshep1size) {
+		this.mshep1size = mshep1size;
+	}
+	public String getMshep1qty() {
+		return mshep1qty;
+	}
+	public void setMshep1qty(String mshep1qty) {
+		this.mshep1qty = mshep1qty;
+	}
+	public String getMshep2size() {
+		return mshep2size;
+	}
+	public void setMshep2size(String mshep2size) {
+		this.mshep2size = mshep2size;
+	}
+	public String getMshep2qty() {
+		return mshep2qty;
+	}
+	public void setMshep2qty(String mshep2qty) {
+		this.mshep2qty = mshep2qty;
+	}
+	public String getMshep3size() {
+		return mshep3size;
+	}
+	public void setMshep3size(String mshep3size) {
+		this.mshep3size = mshep3size;
+	}
+	public String getMshep3qty() {
+		return mshep3qty;
+	}
+	public void setMshep3qty(String mshep3qty) {
+		this.mshep3qty = mshep3qty;
+	}
+	public String getWshep1size() {
+		return wshep1size;
+	}
+	public void setWshep1size(String wshep1size) {
+		this.wshep1size = wshep1size;
+	}
+	public String getWshep1qty() {
+		return wshep1qty;
+	}
+	public void setWshep1qty(String wshep1qty) {
+		this.wshep1qty = wshep1qty;
+	}
+	public String getWshep2size() {
+		return wshep2size;
+	}
+	public void setWshep2size(String wshep2size) {
+		this.wshep2size = wshep2size;
+	}
+	public String getWshep2qty() {
+		return wshep2qty;
+	}
+	public void setWshep2qty(String wshep2qty) {
+		this.wshep2qty = wshep2qty;
+	}
+	public String getWshep3size() {
+		return wshep3size;
+	}
+	public void setWshep3size(String wshep3size) {
+		this.wshep3size = wshep3size;
+	}
+	public String getQtrzipgrey1size() {
+		return qtrzipgrey1size;
+	}
+	public void setQtrzipgrey1size(String qtrzipgrey1size) {
+		this.qtrzipgrey1size = qtrzipgrey1size;
+	}
+	public String getQtrzipgrey1qty() {
+		return qtrzipgrey1qty;
+	}
+	public void setQtrzipgrey1qty(String qtrzipgrey1qty) {
+		this.qtrzipgrey1qty = qtrzipgrey1qty;
+	}
+	public String getQtrzipgrey2size() {
+		return qtrzipgrey2size;
+	}
+	public void setQtrzipgrey2size(String qtrzipgrey2size) {
+		this.qtrzipgrey2size = qtrzipgrey2size;
+	}
+	public String getQtrzipgrey2qty() {
+		return qtrzipgrey2qty;
+	}
+	public void setQtrzipgrey2qty(String qtrzipgrey2qty) {
+		this.qtrzipgrey2qty = qtrzipgrey2qty;
+	}
+	public String getQtrzipgrey3size() {
+		return qtrzipgrey3size;
+	}
+	public void setQtrzipgrey3size(String qtrzipgrey3size) {
+		this.qtrzipgrey3size = qtrzipgrey3size;
+	}
+	public String getQtrzipgrey3qty() {
+		return qtrzipgrey3qty;
+	}
+	public void setQtrzipgrey3qty(String qtrzipgrey3qty) {
+		this.qtrzipgrey3qty = qtrzipgrey3qty;
+	}
+	public String getQtrzipblack1size() {
+		return qtrzipblack1size;
+	}
+	public void setQtrzipblack1size(String qtrzipblack1size) {
+		this.qtrzipblack1size = qtrzipblack1size;
+	}
+	public String getQtrzipblack1qty() {
+		return qtrzipblack1qty;
+	}
+	public void setQtrzipblack1qty(String qtrzipblack1qty) {
+		this.qtrzipblack1qty = qtrzipblack1qty;
+	}
+	public String getQtrzipblack2size() {
+		return qtrzipblack2size;
+	}
+	public void setQtrzipblack2size(String qtrzipblack2size) {
+		this.qtrzipblack2size = qtrzipblack2size;
+	}
+	public String getQtrzipblack2qty() {
+		return qtrzipblack2qty;
+	}
+	public void setQtrzipblack2qty(String qtrzipblack2qty) {
+		this.qtrzipblack2qty = qtrzipblack2qty;
+	}
+	public String getQtrzipblack3size() {
+		return qtrzipblack3size;
+	}
+	public void setQtrzipblack3size(String qtrzipblack3size) {
+		this.qtrzipblack3size = qtrzipblack3size;
+	}
+	public String getQtrzipblack3qty() {
+		return qtrzipblack3qty;
+	}
+	public void setQtrzipblack3qty(String qtrzipblack3qty) {
+		this.qtrzipblack3qty = qtrzipblack3qty;
+	}
+	public String getTshirt1size() {
+		return tshirt1size;
+	}
+	public void setTshirt1size(String tshirt1size) {
+		this.tshirt1size = tshirt1size;
+	}
+	public String getTshirt1qty() {
+		return tshirt1qty;
+	}
+	public void setTshirt1qty(String tshirt1qty) {
+		this.tshirt1qty = tshirt1qty;
+	}
+	public String getTshirt2size() {
+		return tshirt2size;
+	}
+	public void setTshirt2size(String tshirt2size) {
+		this.tshirt2size = tshirt2size;
+	}
+	public String getTshirt2qty() {
+		return tshirt2qty;
+	}
+	public void setTshirt2qty(String tshirt2qty) {
+		this.tshirt2qty = tshirt2qty;
+	}
+	public String getTshirt3size() {
+		return tshirt3size;
+	}
+	public void setTshirt3size(String tshirt3size) {
+		this.tshirt3size = tshirt3size;
+	}
+	public String getTshirt3qty() {
+		return tshirt3qty;
+	}
+	public void setTshirt3qty(String tshirt3qty) {
+		this.tshirt3qty = tshirt3qty;
+	}
+	public String getTotalvalue() {
+		return totalvalue;
+	}
+	public void setTotalvalue(String totalvalue) {
+		this.totalvalue = totalvalue;
+	}
+	public String getWshep3qty() {
+		return wshep3qty;
+	}
+	public void setWshep3qty(String wshep3qty) {
+		this.wshep3qty = wshep3qty;
+	}
+	
+}
--- src/main/java/com/dbpfootball/adsform/rest/controller/Endpoint.java
+++ src/main/java/com/dbpfootball/adsform/rest/controller/Endpoint.java
@@ -1,6 +1,9 @@
 package com.dbpfootball.adsform.rest.controller;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +15,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +26,7 @@ import org.springframework.stereotype.Component;
 import com.dbpfootball.adsform.paypalProcessor;
 import com.dbpfootball.adsform.sesemail;
 import com.dbpfootball.adsform.domain.formsubmission;
+import com.dbpfootball.adsform.domain.vvinesformsubmission;
 
 @Component
 @Path("/ws-sender")
@@ -90,5 +96,131 @@ public class Endpoint {
 			
 			return response;
 		}		
-	
+
+		@POST
+		@Path("/vvines")
+		@Consumes(MediaType.APPLICATION_JSON)
+		@Produces(MediaType.APPLICATION_JSON)
+		public Map<String, String> processVVinesForm(vvinesformsubmission formIn) {
+			Map<String, String> response = new HashMap<String, String>();
+
+			formsubmission ppalformIn = new formsubmission();
+			ppalformIn.setAddress(formIn.getCcaddress());
+			ppalformIn.setCccity(formIn.getCccity());
+			ppalformIn.setCczip(formIn.getCczip());
+			ppalformIn.setCcstate(formIn.getCcstate());
+			ppalformIn.setCcexpmo(formIn.getCcexpmo());
+			ppalformIn.setCcexpyr(formIn.getCcexpyr());
+			ppalformIn.setCcfname(formIn.getCcfname());
+			ppalformIn.setCclname(formIn.getCclname());
+			ppalformIn.setCcnum(formIn.getCcnum().replaceAll("\\D+",""));
+			ppalformIn.setCccvv(formIn.getCccvv());
+			ppalformIn.setCctype(formIn.getCctype());
+			ppalformIn.setAdtypevalue(formIn.getTotalvalue());
+			
+			response = ppal.sendFormPayment(ppalformIn);
+			
+			sesemail emailSender = new sesemail();
+			
+			try {
+				//emailSender.sendEmail();
+				
+				String emailSubject = "DBP Vineyard Vines Order Submission - " +formIn.getContactname()+" - PayPal Result: "+response.get("result");
+				String emailBody = 
+									"Contact Name: "+formIn.getContactname()+"\n"+
+									"Address: "+formIn.getAddress()+"\n"+
+									"Email: "+formIn.getEmail()+"\n"+
+									"Phone: "+formIn.getPhone()+"\n"+
+									"Amount Charged $: "+formIn.getTotalvalue()+"\n"+
+									"Credit Card First Name: "+formIn.getCcfname()+"\n"+
+									"Credit Card Last Name: "+formIn.getCclname()+"\n"+
+									"Credit Card Address: "+formIn.getCcaddress()+"\n"+
+									"Credit Card City: "+formIn.getCccity()+"\n"+
+									"Credit Card State: "+formIn.getCcstate()+"\n"+
+									"Credit Card Zip: "+formIn.getCczip()+"\n"+
+									"Credit Card Type: "+formIn.getCctype()+"\n"+
+									"Credit Card Last 4: "+formIn.getCcnum().substring(formIn.getCcnum().length() - 4)+"\n"+
+									"Credit Card Expiration Month: "+formIn.getCcexpmo()+"\n"+
+									"Credit Card Expiration Year: "+formIn.getCcexpyr()+"\n\n";
+				
+				JSONObject jso = new JSONObject();
+				jso.put("tie1qty", formIn.getTie1qty());
+				jso.put("tie2qty", formIn.getTie2qty());
+				jso.put("hat1qty", formIn.getHat1qty());
+				jso.put("hat2qty", formIn.getHat2qty());
+
+				jso.put("belt1size", formIn.getBelt1size());
+				jso.put("belt1qty", formIn.getBelt1qty());
+				jso.put("belt2size", formIn.getBelt2size());
+				jso.put("belt2qty", formIn.getBelt2qty());
+				jso.put("belt3size", formIn.getBelt3size());
+				jso.put("belt3qty", formIn.getBelt3qty());
+				
+				jso.put("mshep1size", formIn.getMshep1size());
+				jso.put("mshep1qty", formIn.getMshep1qty());
+				jso.put("mshep2size", formIn.getMshep2size());
+				jso.put("mshep2qty", formIn.getMshep2qty());
+				jso.put("mshep3size", formIn.getMshep3size());
+				jso.put("mshep3qty", formIn.getMshep3qty());
+
+				jso.put("wshep1size", formIn.getWshep1size());
+				jso.put("wshep1qty", formIn.getWshep1qty());
+				jso.put("wshep2size", formIn.getWshep2size());
+				jso.put("wshep2qty", formIn.getWshep2qty());
+				jso.put("wshep3size", formIn.getWshep3size());
+				jso.put("wshep3qty", formIn.getWshep3qty());
+
+				jso.put("qtrzipgrey1size", formIn.getQtrzipgrey1size());
+				jso.put("qtrzipgrey1qty", formIn.getQtrzipgrey1qty());				
+				jso.put("qtrzipgrey2size", formIn.getQtrzipgrey2size());
+				jso.put("qtrzipgrey2qty", formIn.getQtrzipgrey2qty());				
+				jso.put("qtrzipgrey3size", formIn.getQtrzipgrey3size());
+				jso.put("qtrzipgrey3qty", formIn.getQtrzipgrey3qty());				
+
+				jso.put("qtrzipblack1size", formIn.getQtrzipblack1size());
+				jso.put("qtrzipblack1qty", formIn.getQtrzipblack1qty());				
+				jso.put("qtrzipblack2size", formIn.getQtrzipblack2size());
+				jso.put("qtrzipblack2qty", formIn.getQtrzipblack2qty());				
+				jso.put("qtrzipblack3size", formIn.getQtrzipblack3size());
+				jso.put("qtrzipblack3qty", formIn.getQtrzipblack3qty());				
+
+				jso.put("tshirt1size", formIn.getTshirt1size());
+				jso.put("tshirt1qty", formIn.getTshirt1qty());
+				jso.put("tshirt2size", formIn.getTshirt2size());
+				jso.put("tshirt2qty", formIn.getTshirt2qty());
+				jso.put("tshirt3size", formIn.getTshirt3size());
+				jso.put("tshirt3qty", formIn.getTshirt3qty());
+				
+				emailBody = emailBody + "\nJSON Order: "+jso.toString()+"\n\n";
+
+				String asciiInsert = jso.toString() + "\n";
+				try {
+				    Files.write(Paths.get("/home/bweschke/vvinesorders.txt"), asciiInsert.getBytes(), StandardOpenOption.APPEND);
+				}catch (IOException e) {
+
+				}				
+				
+				if (response.get("result").equals("failure")) {
+					emailBody = emailBody + "PayPal Error Message for Rejection: "+response.get("errormsg")+"\n";
+				} else {
+					emailBody = emailBody + "PayPal Success Payment ID: "+response.get("paymentid")+"\n";
+				}
+				
+				emailSender.sendEmail("bweschke@btwtech.com", emailBody, emailSubject);
+				emailSender.sendEmail("dbp.tdc.2017ads@gmail.com", emailBody, emailSubject);
+				emailSender.sendEmail("v.kovanes@verizon.net", emailBody, emailSubject);
+				emailSender.sendEmail("keenanclan5@aol.com", emailBody, emailSubject);
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return response;
+		}		
+		
+		
 }
